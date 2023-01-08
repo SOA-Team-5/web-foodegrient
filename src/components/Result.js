4671aeba20c8e602d805cda342c8d1c752239693
demoswx <gamemasterxuan.com>
2023-01-09 00:04:12 +0800

    Update

---
+++ src/components/Result.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Header from "./ResultHeader";
+import thumb from "../asset/thumb-down.png";
+
+async function getCard() {}
+
+function Result() {
+  const [card, setCard] = useState([]);
+  const { search } = useParams();
+  const [menuData, setMenuData] = useState("");
+
+  useEffect(() => {
+    if (menuData === "") {
+      getrecipes();
+    }
+  }, [menuData]);
+
+  const getrecipes = async () => {
+    //   console.log(params);
+    const res = await Axios.get(
+      `https://foodegrient-api.herokuapp.com/api/v1/menu?keywords=${search}`
+    );
+    setMenuData(res.data.recipes);
+  };
+
+  async function handleLikeBtn(id, index) {
+    const res = await Axios.get(
+      `https://foodegrient-api.herokuapp.com/api/v1/like?id=${id}`
+    );
+    let tempMenuData = menuData;
+    tempMenuData[index].likes += 1;
+    setMenuData(tempMenuData);
+    setMenuData(prevList => {
+        prevList[index].checked = !prevList[index].checked // 这里数据引用地址的值同样被修改
+        return [...prevList]})
+  }
+
+  async function handleUnlikeBtn(id, index) {
+    const res = await Axios.get(
+      `https://foodegrient-api.herokuapp.com/api/v1/unlike?id=${id}`
+    );
+    let tempMenuData = menuData;
+    tempMenuData[index].unlikes += 1;
+    setMenuData(tempMenuData);
+    setMenuData(prevList => {
+        prevList[index].checked = !prevList[index].checked // 这里数据引用地址的值同样被修改
+        return [...prevList]})
+  }
+
+  var card_array = [];
+
+  for (let i = 0; i < menuData.length; i++) {
+    card_array.push(
+      <div
+        className="col-md-6 col-sm-6 col-xs-12"
+        style={{
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderColor: "#E4E9F2",
+          marginBottom: "20px",
+          marginInline: "10px",
+          maxWidth: "300px",
+        }}
+      >
+        <div
+          className="row-md-11"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <div className="col-md-11">
+            <img
+              className="card-img-top img-thumbnail"
+              style={{ marginTop: "10px" }}
+              src={menuData[i].image}
+            />
+          </div>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{menuData[i].title}</h5>
+          <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+              <div class="fw-bold"></div>
+              Likes: {menuData[i].likes}{" "}
+            </div>
+            <div onClick={()=>{handleLikeBtn(menuData[i].recipe_id, i)}}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-hand-thumbs-up-fill"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a9.84 9.84 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733.058.119.103.242.138.363.077.27.113.567.113.856 0 .289-.036.586-.113.856-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.163 3.163 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.82 4.82 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
+              </svg>
+              
+            </div>
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+              <div class="fw-bold"></div>
+              Unlikes: {menuData[i].unlikes}{" "}
+            </div>
+            <div onClick={()=>{handleUnlikeBtn(menuData[i].recipe_id, i)}}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-hand-thumbs-down-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6.956 14.534c.065.936.952 1.659 1.908 1.42l.261-.065a1.378 1.378 0 0 0 1.012-.965c.22-.816.533-2.512.062-4.51.136.02.285.037.443.051.713.065 1.669.071 2.516-.211.518-.173.994-.68 1.2-1.272a1.896 1.896 0 0 0-.234-1.734c.058-.118.103-.242.138-.362.077-.27.113-.568.113-.856 0-.29-.036-.586-.113-.857a2.094 2.094 0 0 0-.16-.403c.169-.387.107-.82-.003-1.149a3.162 3.162 0 0 0-.488-.9c.054-.153.076-.313.076-.465a1.86 1.86 0 0 0-.253-.912C13.1.757 12.437.28 11.5.28H8c-.605 0-1.07.08-1.466.217a4.823 4.823 0 0 0-.97.485l-.048.029c-.504.308-.999.61-2.068.723C2.682 1.815 2 2.434 2 3.279v4c0 .851.685 1.433 1.357 1.616.849.232 1.574.787 2.132 1.41.56.626.914 1.28 1.039 1.638.199.575.356 1.54.428 2.591z" />
+            </svg>
+            </div>
+          </li>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <>
+      <Header />
+      <div className="Page1" style={{ marginTop: "20px" }}>
+        <div className="container">
+          <div className="row">{card_array !== [] ? card_array : []}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Result;
