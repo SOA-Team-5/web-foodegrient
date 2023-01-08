e40df0aa6e0f48639d4623d5e1869b242998ad63
demoswx <gamemasterxuan.com>
2023-01-08 23:01:42 +0800

    result

--- src/components/result.js
+++ src/components/result.js
@@ -29,23 +29,40 @@ function Result() {
 
   for (let i = 0; i < menuData.length; i++) {
     card_array.push(
-      <div className="col-md-6 col-sm-6 col-xs-12" 
-       style={{borderStyle: 'solid', borderWidth:'1px', borderColor: '#E4E9F2', marginBottom:'20px',
-      marginInline: '10px', maxWidth: '300px'
-      }}
+      <div
+        className="col-md-6 col-sm-6 col-xs-12"
+        style={{
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderColor: "#E4E9F2",
+          marginBottom: "20px",
+          marginInline: "10px",
+          maxWidth: "300px",
+        }}
       >
-        <div className="row-md-11" style={{ display: "flex", justifyContent: "center",}}>
+        <div
+          className="row-md-11"
+          style={{ display: "flex", justifyContent: "center" }}
+        >
           <div className="col-md-11">
             <img
-              className="card-img-top img-thumbnail" style={{marginTop:'10px'}}
+              className="card-img-top img-thumbnail"
+              style={{ marginTop: "10px" }}
               src={menuData[i].image}
             />
           </div>
         </div>
         <div className="card-body">
           <h5 className="card-title">{menuData[i].title}</h5>
-          <li class="list-group-item">Likes: {menuData[i].likes} <span class="badge bg-primary rounded-pill">14</span></li>
+          <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+              <div class="fw-bold"></div>
+              Likes: {menuData[i].likes}{" "}
+            </div>
+            <span class="badge bg-primary rounded-pill">14</span>
+          </li>
           
+
           <li class="list-group-item">Unlikes: {menuData[i].unlikes}</li>
         </div>
       </div>
@@ -54,7 +71,7 @@ function Result() {
   return (
     <>
       <Header />
-      <div className="Page1" style={{marginTop: '20px'}}>
+      <div className="Page1" style={{ marginTop: "20px" }}>
         <div className="container">
           <div className="row">{card_array !== [] ? card_array : []}</div>
         </div>
